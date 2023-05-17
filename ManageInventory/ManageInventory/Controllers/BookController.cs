a1e58b4ab0cada85090397edcdb73d5f12e662a1
Jorge Machado <machadoggg@gmail.com>
2023-05-17 00:37:23 -0500

    Create crud basic

--- ManageInventory/ManageInventory/Controllers/BookController.cs
+++ ManageInventory/ManageInventory/Controllers/BookController.cs
@@ -1,6 +1,8 @@
 ﻿using ManageInventory.Data;
 using ManageInventory.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace ManageInventory.Controllers
 {
@@ -19,6 +21,62 @@ namespace ManageInventory.Controllers
 
             return View(books);
         }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            List<Editorial> editorialList = _context.Editorials.ToList();
+            ViewBag.Editorial = editorialList;
+
+            Book? book = new Book();
+            return View(book);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            book.IdEditorial = 1;
+            _context.Attach(book);
+            _context.Entry(book).State = EntityState.Added;
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public IActionResult Edit(string Id) 
+        {
+            Book? book = _context.Books.Where(b => b.Isbn == Id).FirstOrDefault();
+            return View(book);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Book book)
+        {
+            _context.Attach(book);
+            _context.Entry(book).State = EntityState.Modified;
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public IActionResult Delete(string Id)
+        {
+            Book? book = _context.Books.Where(b => b.Isbn == Id).FirstOrDefault();
+            return View(book);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Book book)
+        {
+            _context.Attach(book);
+            _context.Entry(book).State = EntityState.Modified;
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
 
         public IActionResult Details(string Id) 
         {
